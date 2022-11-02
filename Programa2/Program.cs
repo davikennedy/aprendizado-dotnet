fd9fecb267bb949c0c0d77df0c60703f262b2489
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-01 21:51:45 -0300

    Adição simples e conversão de dados implícita

---
+++ Programa2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Adição de números inteiros
+int firstNumber = 12;
+int secondNumber = 7;
+Console.Write(firstNumber + secondNumber + "\n");
+
+// Lidando com apresentações no console
+string nome = "Davi";
+int qtdObjetos = 7;
+Console.WriteLine(nome + " comprou " + qtdObjetos + 7 + " objetos."); // Davi comprou 77 objetos.
+Console.WriteLine(nome + " comprou " + (qtdObjetos + 7) + " objetos."); // Davi comprou 14 objetos.
