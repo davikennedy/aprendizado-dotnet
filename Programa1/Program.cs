490f2ec2c7b4fe342c68a563753ca0ee9b20bc59
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-01 21:11:38 -0300

    Aprendendo a utilizar escapes de caracteres, concatenação e interpolação

---
+++ Programa1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Aprendendo a utilizar escapes de caracteres, concatenação e interpolação
+
+Console.WriteLine("Fatura: 1021\t\tCompleta!");
+Console.WriteLine("Fatura: 1022\t\tCompleta!");
+Console.WriteLine("\nDiretório de saída:\t");
+Console.WriteLine(@"c:\faturas");
+
+/* Maneiras de concatenar strings:
+
+string firstName = "Davi";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!"); // Concatenação normal
+
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine($"{greeting} {firstName}!"); // Interpolação
+
+*/
