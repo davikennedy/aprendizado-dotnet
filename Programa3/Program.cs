febd14c7386a8dfadd2d4599af430644cdf88481
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-01 22:14:32 -0300

    Lidando com operadores matemáticos

---
+++ Programa3/Program.cs
@@ -0,0 +1,19 @@
+﻿int soma = 7 + 5;
+int subtracao = 7 - 5;
+int multiplicacao = 7 * 5;
+int divisao = 7 / 5;
+
+Console.WriteLine("Soma: " + soma); // 12
+Console.WriteLine("Subtração: " + subtracao); // 2
+Console.WriteLine("Multiplicação: " + multiplicacao); // 35
+Console.WriteLine("Divisão: " + divisao); // 1
+
+int primeiro = 7;
+int segundo = 5;
+decimal quociente = (decimal)primeiro / (decimal)segundo;
+Console.WriteLine(quociente); // 1.4
+
+int valor1 = 3 + 4 * 5;
+int valor2 = (3 + 4) * 5;
+Console.WriteLine(valor1); // 23
+Console.WriteLine(valor2); // 35
