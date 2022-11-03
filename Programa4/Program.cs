96517226d82cb0e0e4a14d12df22ef3c75313304
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-02 22:33:51 -0300

    Aprendendo incremento e decremento de valores

---
+++ Programa4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Operadores como +=, -=, *=, ++ e -- são conhecidos como operadores de atribuição composta 
+porque compõem uma operação além de atribuir o resultado à variável. O operador += é chamado 
+especificamente de operador de atribuição de adição.
+*/
+
+int valor = 1;
+valor++;
+
+Console.WriteLine("Primeiro: \t" + valor); // 2
+Console.WriteLine("Segundo: \t" + valor++); // 2
+Console.WriteLine("Terceiro: \t" + valor); // 3
+Console.WriteLine("Quarto: \t" + (++valor)); // 4
