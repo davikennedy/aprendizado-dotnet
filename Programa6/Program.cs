fd5628388fb4f2097a4c1303939abe072a0029d5
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-04 22:14:42 -0300

    Resolvendo desafio de chamar um método que retorna o maior de dois números

---
+++ Programa6/Program.cs
@@ -0,0 +1,8 @@
+﻿/* 
+    Desafio: Usar o IntelliSense ou a documentação em docs.microsoft.com para 
+    localizar e chamar um método que retorna o maior de dois números.
+*/
+
+int firstValue = 500;
+int secondValue = 600;
+Console.WriteLine(Math.Max(firstValue, secondValue)); // 600
