3a512c1cb7b5b724fdcc3f047754d7f4ca76ffdf
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-08 21:58:56 -0300

    Aprendendo o uso da instrução Foreach

---
+++ Programa10/Program.cs
@@ -0,0 +1,17 @@
+﻿// string[] nomes = {"Davi", "Késia", "Iago"};
+// 
+// foreach (string nome in nomes) {
+//     Console.WriteLine(nome);
+// }
+
+int[] inventario = { 200, 450, 700, 175, 250 };
+int soma = 0;
+int caixa = 0;
+
+foreach (int itens in inventario) {
+	  soma += itens;
+		caixa++;
+    Console.WriteLine($"Caixa {caixa} = {itens} itens (Execução total: {soma})");
+}
+
+Console.WriteLine($"Nós temos {soma} itens no inventário.");
