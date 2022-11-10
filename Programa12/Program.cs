86d546319037ae782e02dcc9f4cb07cab6ed6106
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-09 21:08:11 -0300

    Lidando com expressões booleanas

---
+++ Programa12/Program.cs
@@ -0,0 +1,15 @@
+﻿// Usando o operador de igualdade
+string valor1 = "a ";
+string valor2 = "A";
+
+Console.WriteLine(valor1 == valor2); // Saída: false;
+Console.WriteLine(valor1.Trim().ToLower() == valor2.Trim().ToLower()); // Saída: True
+
+
+// Usando o operador de desigualdade
+Console.WriteLine("a" != "a"); // Saída: False
+Console.WriteLine("a" != "A"); // Saída: True
+Console.WriteLine(1 != 2); // Saída: False
+
+string meuValor = "a";
+Console.WriteLine(meuValor != "a"); // Saída: False
