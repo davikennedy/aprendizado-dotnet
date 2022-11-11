8d6813084d52e9a59365afdeb71b80e691282586
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-10 21:01:58 -0300

    Resolvendo desafio de 'Cara' ou 'Coroa' :rocket:

---
+++ Programa13/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+    Desafio: Lançar moeda para exibir "Cara" ou "Coroa".
+    As chances de que o resultado seja "Cara" ou "Coroa" devem ser de 50% cada.
+    Você deve ser capaz de realizar o resultado desejado em três linhas de código ou menos.
+*/
+
+Random randomico = new Random();
+int teste = randomico.Next(0, 2);
+Console.WriteLine($"Teste: {(teste == 0 ? "Cara" : "Coroa")}");
