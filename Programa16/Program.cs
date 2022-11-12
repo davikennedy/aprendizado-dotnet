36054d5745f936c467162c8082017c50c93657ef
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-12 09:34:58 -0300

    Aprendendo a remover blocos de código em instruções 'if'

---
+++ Programa16/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Aprendizado obtido: É possível reduzir linhas de código se a instrução tiver apenas uma linha
+*/
+
+string nome = "steve";
+
+if (nome == "bob") 
+    Console.WriteLine("Found Bob");
+else if (nome == "steve") 
+    Console.WriteLine("Found Steve");
+else 
+    Console.WriteLine("Found Chuck");
