83c075ef648154b1fc5d0c8b25f17683fb03294a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-11 23:06:21 -0300

    Aprendendo a lidar com Namespaces, Classes e Métodos

---
+++ Programa15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/*  using MeuNovoApp.Utilitarios;
+    Dessa forma, a linha 13 poderia ser reduzida para "string fraseReversa = Utilitario.Reverse(frase);"
+*/
+
+namespace MeuNovoApp 
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Microsoft Learn";
+            string fraseReversa = MeuNovoApp.Utilitarios.Utilitario.Reverse(frase);
+            Console.WriteLine(fraseReversa);
+        }
+    }
+}
+
+namespace MeuNovoApp.Utilitarios
+{
+    class Utilitario
+    {
+        public static string Reverse(string mensagem)
+        {
+            char[] letras = mensagem.ToCharArray();
+            Array.Reverse(letras);
+            return new string(letras);
+        }
+    }
+}
