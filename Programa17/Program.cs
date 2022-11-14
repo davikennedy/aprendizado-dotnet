8c96cff873c23cd280f3e5842e6b92e899c80b20
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-13 22:09:47 -0300

    Aprendendo a utilizar a instrução 'switch'

---
+++ Programa17/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Aprendendo a utilizar a instrução switch
+*/
+
+int empregadoNivel = 200;
+string empregadoNome = "João Alves";
+
+string titulo = "";
+
+switch (empregadoNivel) {
+    case 100:
+        titulo = "Associado Júnior";
+        break;
+    case 200:
+        titulo = "Associado Sênior";
+        break;
+    case 300:
+        titulo = "Gerente";
+        break;
+    case 400:
+        titulo = "Gerente Sênior";
+        break;
+    default: // Não é obrigatório definir um rótulo 'default', mas é muito útil para utilizar como "opção padrão" ou mesmo, como "última opção"
+        titulo = "Associado";
+        break;
+}
+
+Console.WriteLine($"{empregadoNome} - {titulo}");
