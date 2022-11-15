3e6c833e2344a894ee2a1324bdd7834fe78fd25e
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-14 22:13:25 -0300

    Resolvendo o desafio: Implementando as regras do FizzBuzz :white_check_mark:

---
+++ Programa19/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Desafio: Implementar as regras do FizzBuzz:
+
+    Imprima valores de saída de 1 a 100, um número por linha.
+    Quando o valor atual é divisível por 3, imprima o termo Fizz ao lado do número.
+    Quando o valor atual é divisível por 5, imprima o termo Buzz ao lado do número.
+    Quando o valor atual é divisível tanto por 3 quanto por 5, imprima o termo FizzBuzz ao lado do número.
+*/
+
+for (int i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        Console.WriteLine($"{i} - FizzBuzz");
+        continue;
+    }
+    
+    else if (i % 3 == 0) {
+        Console.WriteLine($"{i} - Fizz");
+        continue;
+    } 
+    else if (i % 5 == 0) {
+        Console.WriteLine($"{i} - Buzz");
+        continue;
+    }
+
+    Console.WriteLine(i);
+}
