0ebbb52c2962e7b08f286843fb447d88fc7ef03c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-19 22:58:08 -0300

    Resolvendo desafio de uso do TryParse()

---
+++ Programa24/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Desafio: 
+    Adicione o código necessário para implementar as seguintes regras de negócio:
+
+    Regra 1: se o valor for alfanumérico, concatene-o para formar uma mensagem
+    Regra 2: se o valor for numérico, adicione-o ao total
+    Regra 3: verifique se o resultado corresponde à seguinte saída:
+
+    Comece com a seguinte linha de código:
+    string[] valores = { "12.3", "45", "ABC", "11", "DEF" };
+*/
+
+string[] valores = { "12.3", "45", "ABC", "11", "DEF" };
+
+string mensagem = "";
+double somatorio = 0;
+double numero = 0;
+
+for (int i = 0; i < valores.Length; i++) {
+    if (double.TryParse(valores[i], out numero)) {
+       somatorio += numero;
+    }
+    else {
+        mensagem += valores[i];
+    }
+}
+
+Console.WriteLine($"Mensagem {mensagem}");
+Console.WriteLine($"Total: {somatorio}");
