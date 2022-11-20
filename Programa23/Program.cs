483d6efccf5b07a9bb9611209aba392f05c385e0
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-19 22:24:15 -0300

    Aprendendo a utilizar o método TryParse()

---
+++ Programa23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Aprendendo a utilizar o método TryParse()
+string value = "120";
+int resultado = 0;
+if (int.TryParse(value, out resultado)) {
+    Console.WriteLine($"Teste: {resultado}"); // 120
+}
+else {
+    Console.WriteLine("O teste não foi bem-sucedido.");
+}
