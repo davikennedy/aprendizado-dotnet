9c8e2833ff76b25e5780a9ddda6b12a3769a440e
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-20 22:35:46 -0300

    Aprendendo a utilizar Array.Sort() e Array.Reverse()

---
+++ Programa25/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] paletes = { "B14", "A11", "B12", "A13" };
+
+// Ordenar de forma alfanumérica
+Console.WriteLine("Ordenado: ");
+Array.Sort(paletes);
+foreach (var palete in paletes) {
+    Console.WriteLine($"-- {palete}");
+}
+
+// Inverter a ordem
+Console.WriteLine("\nRevertido:");
+Array.Reverse(paletes);
+foreach(var palete in paletes) {
+    Console.WriteLine($"-- {palete}");
+}
