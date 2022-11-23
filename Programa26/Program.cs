fce3b760c46760acc0b31d457bf9cc611c1773be
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-22 22:49:03 -0300

    Resolvendo desafio de Fibonacci

---
+++ Programa26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Imprimir a sequência de Fibonacci (até o vigésimo elemento)
+
+int atual = 1;
+int anterior = 1;
+
+for (int i = 1; i < 20; i++) {
+    if (i == 1) {
+        Console.WriteLine(anterior);
+    }
+    Console.WriteLine(atual);
+    atual += anterior;
+    anterior = atual - anterior;
+}
