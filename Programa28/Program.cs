124377830c9f9994ce3e83a594d055af018e31d5
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-27 21:42:27 -0300

    Aprendendo a utilizar Array.Clear()

---
+++ Programa28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        string[] paletes = { "B14", "A11", "B12", "A13" };
+        Console.WriteLine("");
+
+        // O Array.Clear serve para limpar elementos de um array, dentro da sequência informada
+        Array.Clear(paletes, 0, 2);
+        Console.WriteLine($"Limpando 2 ... Quantidade: {paletes.Length}");
+
+        foreach (var palete in paletes)
+        {
+            Console.WriteLine($"-- {palete}");
+        }
+
+        Console.WriteLine("Tecle Enter para encerrar");
+        Console.ReadLine();
+    }
+}
