70b0e10465a76be19b16d07afbb0b38985f5c9de
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-03 10:38:54 -0300

    Resolvendo desafio de obter pedidos iniciados com a letra 'B'

---
+++ Programa29/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Desafio: 
+    Escrever um novo código que vai gerar novas IDs de pedido para os pedidos cuja ID começa com a letra "B", 
+    a partir da string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] resultado = orderStream.Split(',');
+
+for (int i = 0; i < resultado.Length; i++)
+{
+    char[] arrayValor = resultado[i].ToCharArray();
+    if (arrayValor[0] == 'B')
+    {
+        Console.WriteLine(resultado[i]);
+        continue;
+    }
+}
