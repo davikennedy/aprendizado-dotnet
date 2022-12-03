370de29aedd2f755a0c1a8d96ffa5d377c129b97
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 21:43:30 -0300

    Aprendendo a utilizar String.Split() e String.Join()

---
+++ Programa27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Há vários métodos e técnicas que permitem converter uma cadeia de caracteres em uma matriz e vice-versa.
+    Use métodos como ToCharArray() e Split() para criar uma matriz.
+    Use métodos como Join() ou crie uma cadeia de caracteres passando uma matriz de char para transformar a matriz novamente em uma só cadeia de caracteres.
+*/
+
+string valor = "abc123";
+char[] arrayValor = valor.ToCharArray();
+
+// String.Join serve para criar uma string com base em um delimitador informado
+string resultado = String.Join(",", arrayValor);
+Console.WriteLine(resultado);
+
+//String.Split serve para criar um array de strings com base em um delimitador informado
+string[] itens = resultado.Split(',');
+foreach (string item in itens)
+{
+    Console.WriteLine(item);
+}
