c2ce5000d57347d2e1ce17537fe92fab364746e8
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-04 23:22:44 -0300

    Aprendendo a utilizar String.Format()

---
+++ Programa30/Program.cs
@@ -0,0 +1,4 @@
+﻿string primeira = "Hello";
+string segunda = "World";
+string resultado = string.Format("{0} {1}!", primeira, segunda); // Saída: Hello World
+Console.WriteLine("{0} {0} {0}!", primeira, segunda); // Saída: Hello Hello Hello
