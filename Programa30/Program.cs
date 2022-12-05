06d18f97ae3237e02a48f7c26fb7c4266408c4f2
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-04 23:57:26 -0300

    Aprendendo a lidar com formatação de números

--- Programa30/Program.cs
+++ Programa30/Program.cs
@@ -1,4 +1,23 @@
-﻿string primeira = "Hello";
+﻿using Internal;
+string primeira = "Hello";
 string segunda = "World";
-string resultado = string.Format("{0} {1}!", primeira, segunda); // Saída: Hello World
-Console.WriteLine("{0} {0} {0}!", primeira, segunda); // Saída: Hello Hello Hello
+string resultado = string.Format("{0} {1}!", primeira, segunda); 
+Console.WriteLine(resultado); // Saída - Hello World
+Console.WriteLine("{0} {0} {0}!", primeira, segunda); // Saída - Hello Hello Hello
+
+
+// Formatar moeda conforme o país
+decimal preco = 123.45m;
+int desconto = 50;
+Console.WriteLine($"Preço: {preco:C} (Desconto {desconto:C})"); // Saída - R$ 123,45 (Desconto R$ 50,00)
+
+
+// Formatar número incluindo vírgulas para delinear, milhares, milhões, etc
+decimal medida = 123456.78912m;
+Console.WriteLine($"Medida: {medida:N}"); // Saída - Medida: 123.456,79
+
+
+// Formatar número com apresentação de percentual
+double taxa = .3678;
+Console.WriteLine($"Taxa: {taxa:P2}"); // Saída - Taxa: 36,78%
+Console.WriteLine($"Taxa: {taxa:P3}"); // Saída - Taxa: 36,780%
