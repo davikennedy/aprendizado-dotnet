062ea29ec7f0542dd53dd319eb902803782c6f4d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-18 23:02:46 -0300

    Aprendendo a utilizar IndexOf()

---
+++ Programa34/Program.cs
@@ -0,0 +1,8 @@
+﻿// IndexOf() retorna um int da posição de um caractere na string
+string mensagem = "Encontre o que está (dentro dos parênteses)";
+
+int aberturaPosicao = mensagem.IndexOf('(');
+int fechamentoPosicao = mensagem.IndexOf(')');
+
+Console.WriteLine(aberturaPosicao);
+Console.WriteLine(fechamentoPosicao);
