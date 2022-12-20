764be1af4c84d116365cf27973d0b574bb02439e
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-19 23:19:46 -0300

    Aprendendo a utilizar Replace()

--- Programa35/Program.cs
+++ Programa35/Program.cs
@@ -1,3 +1,8 @@
 ﻿string dados = "12345DaviKennedy         5000  3  ";
 string dadosAtualizados = dados.Remove(5, 20);
 Console.WriteLine(dadosAtualizados); // Saída: 123455000  3
+
+string mensagem = "Isto--é--um--ex-em-pl-o.";
+mensagem = mensagem.Replace("--", " ");
+mensagem = mensagem.Replace("-", "");
+Console.WriteLine(mensagem); // Saída: Isto é um exemplo
