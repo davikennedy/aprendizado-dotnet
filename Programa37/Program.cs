9004f9d0c824ed8b6a70ed7d5a7ed3c2e71b0993
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-26 23:05:19 -0300

    Aprendendo a utilizar expressões regulares com Regex

---
+++ Programa37/Program.cs
@@ -0,0 +1,8 @@
+﻿// Aprendendo a utilizar expressões regulares com Regex
+using System.Text.RegularExpressions;
+
+string padraoTelefone = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+string texto = "Meu número de contato é 8574-9741";
+
+Match telefoneObtido = Regex.Match(texto, padraoTelefone);
+System.Console.WriteLine(telefoneObtido.Value);
