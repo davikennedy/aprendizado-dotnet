ad00727d89c08084c67fd57e41585c9680ce9836
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-27 11:13:41 -0300

    Aprendendo a utilizar quantificadores nas expressões regulares

--- Programa37/Program.cs
+++ Programa37/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Aprendendo a utilizar expressões regulares com Regex
 using System.Text.RegularExpressions;
 
-string padraoTelefone = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+//string padraoTelefone = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+//string padraoTelefone = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+string padraoTelefone = "[0-9]{4}[-][0-9]{4}";
 string texto = "Meu número de contato é 8574-9741";
 
 Match telefoneObtido = Regex.Match(texto, padraoTelefone);
