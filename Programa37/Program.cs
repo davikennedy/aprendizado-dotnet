f67a9897ad9e8a8a096fa738f89a599faeda0eac
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-27 12:40:25 -0300

    Aprendendo a utilizar quantificadores com intervalos, nas expressões regulares

--- Programa37/Program.cs
+++ Programa37/Program.cs
@@ -3,8 +3,11 @@ using System.Text.RegularExpressions;
 
 //string padraoTelefone = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
 //string padraoTelefone = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
-string padraoTelefone = "[0-9]{4}[-][0-9]{4}";
-string texto = "Meu número de contato é 8574-9741";
+//string padraoTelefone = "[0-9]{4,5}[-][0-9]{4}";
+//string padraoTelefone = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+//string padraoTelefone = "[0-9]{4,5}-{0,1}[0-9]{4}";
+string padraoTelefone = "[0-9]{4,5}-?[0-9]{4}";
+string texto = "Meu número de contato é 98574-9741";
 
 Match telefoneObtido = Regex.Match(texto, padraoTelefone);
 System.Console.WriteLine(telefoneObtido.Value);
