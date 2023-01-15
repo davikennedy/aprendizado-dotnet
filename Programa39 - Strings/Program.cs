46b13d0b472cfee8277db64cad2fa46060b9813c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-14 21:30:21 -0300

    Aprendendo a lidar com Guids

---
+++ Programa39 - Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿// Criando id's com o uso de Guid
+var id = Guid.NewGuid();
+id.ToString();
+
+id = new Guid("c618ef17-931a-4dc4-9adc-6dd1c78baf0d");
+
+id = new Guid(); // Esse código vai atribuir o valor 00000000-0000-0000-0000-000000000000 para o id
+
+Console.WriteLine(id);
