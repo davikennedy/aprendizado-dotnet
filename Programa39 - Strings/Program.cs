0dd50f3ce39a47575598d579260df79a045a55da
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-15 21:57:35 -0300

    Aprendendo métodos de comparação de strings

--- Programa39 - Strings/Program.cs
+++ Programa39 - Strings/Program.cs
@@ -3,7 +3,20 @@ var id = Guid.NewGuid();
 id.ToString();
 
 id = new Guid("c618ef17-931a-4dc4-9adc-6dd1c78baf0d");
-
 id = new Guid(); // Esse código vai atribuir o valor 00000000-0000-0000-0000-000000000000 para o id
 
 Console.WriteLine(id);
+
+
+// Comparando strings
+var texto = "Testando";
+Console.WriteLine(texto.CompareTo("Testando")); // Retorna 0, quando a string comparada é idêntica
+Console.WriteLine(texto.Contains("estando")); // Retorna True se a correspondência for encontrada
+
+var texto2 = "Teste";
+
+var teste1 = texto2.Contains("teste");
+Console.WriteLine(teste1); // Retorna False
+
+var teste2 = texto2.Contains("teste", StringComparison.OrdinalIgnoreCase);
+Console.WriteLine(teste2); // Retorna True
