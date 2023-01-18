976d10a39225507cad1db6a0cf4512b5cab36c8a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-17 23:33:53 -0300

    Aprendendo métodos de manipulação de strings

--- Programa39 - Strings/Program.cs
+++ Programa39 - Strings/Program.cs
@@ -23,13 +23,31 @@ Console.WriteLine(teste2); // Retorna True
 
 
 // StartsWith / EndsWith
-
 var texto3 = "Este texto é um teste";
 
-System.Console.WriteLine(texto3.StartsWith("Este")); // Retorna True
-System.Console.WriteLine(texto3.StartsWith("este")); // Retorna False
-System.Console.WriteLine(texto3.StartsWith("este", StringComparison.OrdinalIgnoreCase)); // Retorna True
+Console.WriteLine(texto3.StartsWith("Este")); // Retorna True
+Console.WriteLine(texto3.StartsWith("este")); // Retorna False
+Console.WriteLine(texto3.StartsWith("este", StringComparison.OrdinalIgnoreCase)); // Retorna True
 
-System.Console.WriteLine(texto3.EndsWith("teste")); // Retorna True
-System.Console.WriteLine(texto3.EndsWith("Teste")); // Retorna False
-System.Console.WriteLine(texto3.EndsWith("Teste", StringComparison.OrdinalIgnoreCase)); // Retorna True
+Console.WriteLine(texto3.EndsWith("teste")); // Retorna True
+Console.WriteLine(texto3.EndsWith("Teste")); // Retorna False
+Console.WriteLine(texto3.EndsWith("Teste", StringComparison.OrdinalIgnoreCase)); // Retorna True
+
+// Manipulando strings
+Console.WriteLine(texto3.Replace("e", "X")); // Retorna EstX tXxto é um tXstX
+Console.WriteLine(texto3.Substring(startIndex: 5, length: 5)); // Retorna texto
+System.Console.WriteLine(texto3.Insert(5, "AQUI ")); // Insere o texto AQUI na posição 5 da string
+System.Console.WriteLine(texto3.Remove(5, 5)); // Remove 5 caracteres a partir do 5º caractere da string
+
+texto3 = "   Este texto é um teste   ";
+texto3 = texto3.Trim();
+Console.WriteLine($"Texto com trim: {texto3}"); // O trim remove os espaços em branco no início e fim de uma string
+
+// Usando método split
+var divisao = texto3.Split(" ");
+Console.WriteLine(divisao[0]);
+
+foreach (string div in divisao)
+{
+    Console.WriteLine(div);
+}
