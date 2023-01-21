fc42f6222dd0324d96106bf0e524419b2331ac8d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-21 16:42:52 -0300

    Aprendendo a utilizar métodos da classe Math

---
+++ Programa42/Program.cs
@@ -0,0 +1,10 @@
+﻿decimal valor = 10536.25m;
+
+// Retornar valor arredondado
+System.Console.WriteLine(Math.Round(valor)); // Saída: 10536
+
+// Retornar valor arredondado para cima
+System.Console.WriteLine(Math.Ceiling(valor)); // Saída: 10537
+
+// Retornar valor arredondado para baixo
+System.Console.WriteLine(Math.Floor(valor)); // Saída: 10536
