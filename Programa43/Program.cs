b407843ed75b203499fb4660ee8b8d6fb43f4ef9
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-27 23:05:35 -0300

    Aprendendo formatos de Data/Hora

---
+++ Programa43/Program.cs
@@ -0,0 +1,10 @@
+﻿var data = DateTime.Now;
+// var dataFormatada = String.Format("{0:T}", data); // 22:58:06 (Exemplo)
+// var dataFormatada = String.Format("{0:t}", data); // 22:57 (Exemplo)
+// var dataFormatada = String.Format("{0:d}", data); // sexta-feira, 27 de janeiro de 2023 (Exemplo)
+// var dataFormatada = String.Format("{0:R}", data); // Fri, 27 Jan 2023 23:01:28 GMT (Exemplo)
+// var dataFormatada = String.Format("{0:r}", data); // Fri, 27 Jan 2023 23:04:13 GMT (Exemplo)
+// var dataFormatada = String.Format("{0:s}", data); // 2023-01-27T23:04:40 (Exemplo)
+var dataFormatada = String.Format("{0:u}", data); // 2023-01-27 23:05:04Z (Exemplo)
+
+Console.WriteLine(dataFormatada);
