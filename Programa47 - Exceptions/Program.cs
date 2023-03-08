3c8e11a50ac831f177cf6529fd28e054ab62baf3
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-07 22:33:21 -0300

    Realizando tratamento inicial de Exception

---
+++ Programa47 - Exceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿var array = new int[3];
+
+try
+{
+    for (int index = 0; index < 10; index++)
+    {
+        Console.WriteLine(array[index]);
+    }
+}
+catch
+{
+    Console.WriteLine("Ops, algo deu errado!");
+}
