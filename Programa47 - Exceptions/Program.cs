47f6fe58fdb85c4711fa3ee21c1579f52d5eb739
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-09 18:46:47 -0300

    Tratando exceções com ArgumentNullException

--- Programa47 - Exceptions/Program.cs
+++ Programa47 - Exceptions/Program.cs
@@ -2,20 +2,33 @@
 
 try
 {
-    for (int index = 0; index < 10; index++)
+    /* for (int index = 0; index < 10; index++)
     {
         Console.WriteLine(array[index]);
-    }
+    } */
+    Cadastrar("");
 }
 catch (IndexOutOfRangeException e)
 {
     Console.WriteLine(e.InnerException);
     Console.WriteLine(e.Message);
-    Console.WriteLine("O indíce informado não existe na list");
+    Console.WriteLine("O índice informado não existe na list");
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine(e.InnerException);
+    Console.WriteLine(e.Message);
+    Console.WriteLine("Insira um texto!");
 }
 catch (Exception e)
 {
     Console.WriteLine(e.InnerException);
     Console.WriteLine(e.Message);
     Console.WriteLine("Ops, algo deu errado!");
+}
+
+static void Cadastrar(string texto)
+{
+    if (string.IsNullOrEmpty(texto))
+        throw new ArgumentNullException("O texto não pode ser nulo ou vazio.");
 }
