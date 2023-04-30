9a905e80e4e8c6a7f37b08072acacd23cb33ba31
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-30 19:39:36 -0300

    Utilizando a classe Pessoa

--- Programa48 - Estrutura de Projetos/Fundamentos/Program.cs
+++ Programa48 - Estrutura de Projetos/Fundamentos/Program.cs
@@ -4,5 +4,10 @@ Pessoa p1 = new Pessoa();
 p1.Nome = "Joãozinho";
 p1.Idade = 25;
 
+Pessoa p2 = new Pessoa();
+p2.Nome = "Mariazinha";
+p2.Idade = 17;
+
 p1.Apresentar();
+p2.Apresentar();
 Console.ReadKey();
