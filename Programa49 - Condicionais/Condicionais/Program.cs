bdd66470f1eb635e0d49813797b1aef62367bdeb
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-02 22:41:25 -0300

    Testando condicionais

--- Programa49 - Condicionais/Condicionais/Program.cs
+++ Programa49 - Condicionais/Condicionais/Program.cs
@@ -2,10 +2,23 @@
 
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
 Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
 Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida.");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda realizda.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
 
 Console.ReadKey();
