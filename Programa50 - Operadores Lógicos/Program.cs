7eee5200e71ad095d02c6230962ac681a6f8b5aa
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-03 08:14:57 -0300

    Usando operadores lógicos

---
+++ Programa50 - Operadores Lógicos/Program.cs
@@ -0,0 +1,11 @@
+﻿bool ehMaiorDeIdade = false;
+bool possuiAutorizacaoDoResponsavel = true;
+
+if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+    Console.WriteLine("Entrada liberada.");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada.");
+}
