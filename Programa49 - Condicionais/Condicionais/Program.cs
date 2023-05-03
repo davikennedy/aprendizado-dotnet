150360dc4e7a8b3f416f19cdfd80b496f4a01f2c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-02 22:27:55 -0300

    Usando condicionais

---
+++ Programa49 - Condicionais/Condicionais/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+Console.ReadKey();
