937e0bf53d7e06e0d3ce62b9e62460abebf5e969
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-04 19:25:48 -0300

    Adicionando Classe Calculadora

---
+++ Programa48 - Estrutura de Projetos/Fundamentos/Models/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+    }
+}
--- Programa48 - Estrutura de Projetos/Fundamentos/Program.cs
+++ Programa48 - Estrutura de Projetos/Fundamentos/Program.cs
@@ -1,9 +1,21 @@
 ﻿using Fundamentos.Models;
 
+Calculadora calculadora = new Calculadora();
+calculadora.Somar(10, 30);
+calculadora.Subtrair(35, 10);
+calculadora.Multiplicar(5, 7);
+calculadora.Dividir(30, 3);
+
+
+
+
+
+
+/*
 Pessoa p1 = new Pessoa("Joãozinho", 25);
 Pessoa p2 = new Pessoa("Mariazinha", 17);
-
 
 p1.Apresentar();
 p2.Apresentar();
 Console.ReadKey();
+*/
