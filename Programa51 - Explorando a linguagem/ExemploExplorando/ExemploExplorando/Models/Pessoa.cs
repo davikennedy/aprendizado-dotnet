7f83715ce066f24f201d2e7f5004ef2b132e7074
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-06 17:27:23 -0300

    Criando classe Pessoa

---
+++ Programa51 - Explorando a linguagem/ExemploExplorando/ExemploExplorando/Models/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+
+        public string Nome 
+        { 
+            get => _nome.ToUpper();
+
+            set
+            {
+                if (value == "")
+                    throw new ArgumentException("O nome não pode ser vazio.");
+
+                _nome = value;
+            }
+        }
+        
+        public int Idade 
+        {
+            get => _idade;
+            
+            set
+            {
+                if (value < 0) 
+                    throw new ArgumentException("A idade não pode ser menor que zero.");
+
+                _idade = value;
+            } 
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Programa51 - Explorando a linguagem/ExemploExplorando/ExemploExplorando/Program.cs
@@ -0,0 +1,7 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Joãozinho";
+p1.Idade = 5;
+p1.Apresentar();
