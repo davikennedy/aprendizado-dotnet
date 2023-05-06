5d5f5975ed3e438038c2da0dfd972f1e876e5afd
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-06 16:17:09 -0300

    Percorrendo um array com foreach

--- Programa46 - Arrays/Program.cs
+++ Programa46 - Arrays/Program.cs
@@ -4,7 +4,17 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+Console.WriteLine("Percorrendo um array com 'for'");
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição nº {contador} - {arrayInteiros[contador]}");
+}
+
+
+Console.WriteLine("Percorrendo um array com 'foreach'");
+int contadorForeach = 0;
+foreach (var valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição nº {contadorForeach} - {valor}");
+    contadorForeach++;
 }
