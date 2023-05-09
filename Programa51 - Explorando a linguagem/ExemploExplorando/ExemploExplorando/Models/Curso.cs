c9deb782b8f9c6bea84dbb9b4b99892d4e87c122
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-09 20:31:46 -0300

    Refatorando método ListarAlunos

--- Programa51 - Explorando a linguagem/ExemploExplorando/ExemploExplorando/Models/Curso.cs
+++ Programa51 - Explorando a linguagem/ExemploExplorando/ExemploExplorando/Models/Curso.cs
@@ -29,9 +29,11 @@ namespace ExemploExplorando.Models
         public void ListarAlunos()
         {
             Console.WriteLine($"Alunos do curso de {Nome}:\n");
-            foreach (var aluno in Alunos)
+
+            for (int count = 0; count < Alunos.Count; count++)
             {
-                Console.WriteLine(aluno.NomeCompleto);
+                string texto = $"Nº {count + 1} - {Alunos[count].NomeCompleto}";
+                Console.WriteLine(texto);
             }
         }
     }
