ef601652c8e0de7eadaf2a0b46971a91339d575d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-18 23:50:17 -0300

    Adicionando projeto de hospedagem de hotel

---
+++ Programa53/Models/Reserva.cs
@@ -0,0 +1,49 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count > Suite.Capacidade)
+            {
+                throw new ArgumentException("Capacidade máxima atingida. Não é possível adicionar mais hóspedes.");
+
+            }
+
+            Hospedes = hospedes;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
+            }
+
+            return valor;
+        }
+    }
+}
